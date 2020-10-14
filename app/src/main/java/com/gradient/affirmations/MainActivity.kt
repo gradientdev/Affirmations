a41eef41c5d42d0acc2698c6cc2d2d79f3a2058a
gradientdev <xinyunzhouu@gmail.com>
2020-10-14 11:37:06 -0400

    Updated kotlin code, layouts, added 10 images, made new app icon

--- app/src/main/java/com/gradient/affirmations/MainActivity.kt
+++ app/src/main/java/com/gradient/affirmations/MainActivity.kt
@@ -2,10 +2,22 @@ package com.gradient.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.data.Datasource
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.setHomeAsUpIndicator(R.drawable.ic_moon)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+
     }
 }
